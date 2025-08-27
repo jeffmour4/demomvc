fafbf4d8477632e7209eed5f056925bf54ee004a
jeffmour4 <jefflbmoura@gmail.com>
2025-08-26 21:40:37 -0300

    first commit

---
+++ src/main/java/com/example/demomvc/controller/TarefaController.java
@@ -0,0 +1,62 @@
+package com.example.demomvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demomvc.entity.Tarefa;
+import com.example.demomvc.service.TarefaService;
+
+@Controller
+@RequestMapping("/tarefas")
+public class TarefaController {
+	
+	@Autowired
+	private TarefaService service;
+	
+	@GetMapping("/cadastro")
+	public String cadastro(Tarefa tarefa) {
+		return "/tarefa/cadastro";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Tarefa tarefa) {
+		service.salvar(tarefa);
+		
+		return "redirect:/tarefas/lista";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Tarefa tarefa) {
+
+		service.editar(tarefa);
+		return "redirect:/tarefas/cadastro";
+		
+		
+	}
+
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model ) {
+		service.excluir(id);
+		return lista(model);
+		
+	}
+
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("tarefa", service.buscarPorId(id));
+		
+		return "/tarefa/cadastro";
+	}
+	
+	@GetMapping("/lista")
+	public String lista(ModelMap model) {
+		model.addAttribute("tarefas", service.buscaTodos());
+		return "tarefa/lista";
+	}
+}
+
+
---
+++ src/main/java/com/example/demomvc/entity/Tarefa.java
@@ -0,0 +1,44 @@
+package com.example.demomvc.entity;
+
+import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TAREFA")
+public class Tarefa extends AbstractEntity<Long>{
+
+	@Column(name="nome", nullable=false, unique=true, length=60)
+	private String nome;
+	
+	@Column(name="data_entrega", nullable=false, columnDefinition="DATE")
+	@DateTimeFormat(iso=DateTimeFormat.ISO.DATE)
+	private LocalDate dataEntrega;
+	
+	@Column(name="responsavel", nullable=false, length=60)
+	private String responsavel;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	
+	
+}
